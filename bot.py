488e0075da62b06ad27ab2a0985a18da2bc03261
Dahir-ke <ahmeddahir1115@gmail.com>
2026-04-14 10:46:49 +0300

    changes of bot.py4

--- bot.py
+++ bot.py
@@ -18,9 +18,9 @@ from sklearn.calibration import CalibratedClassifierCV
 # ==========================================================
 # CONFIGURATION
 # ==========================================================
-MT5_LOGIN = 134084924
+MT5_LOGIN = 435112321
 MT5_PASSWORD = "Dahir@2036"
-MT5_SERVER = "ExnessKE-MT5Real9"
+MT5_SERVER = "ExnessKE-MT5Trial9"
 
 SYMBOLS = [
     "EURUSDm", "USDJPYm", "XAUUSDm",          # Major + Gold
