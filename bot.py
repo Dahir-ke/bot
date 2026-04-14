fa0ad3eb2564f51e2bd432a1e30492221862f618
Dahir-ke <ahmeddahir1115@gmail.com>
2026-04-14 11:24:54 +0300

    changes of bot.py8

--- bot.py
+++ bot.py
@@ -44,9 +44,9 @@ TRADE_ACTION_SLTP = 6
 MAGIC_NUMBER = 20240601
 
 # ====================== CONFIG ======================
-MT5_LOGIN = 134084924
+MT5_LOGIN = 435112321
 MT5_PASSWORD = "Dahir@2036"
-MT5_SERVER = "ExnessKE-MT5Real9"
+MT5_SERVER = "ExnessKE-MT5Trial9"
 
 SYMBOLS = ["EURUSDm", "USDJPYm", "XAUUSDm", "UKOILm", "USOILm", "XNGUSDm",
            "AUDCADm", "AUDCHFm", "AUDJPYm"]
